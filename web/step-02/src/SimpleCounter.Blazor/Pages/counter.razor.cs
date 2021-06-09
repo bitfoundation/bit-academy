2d07111e470347d59dcd5a5e5554edc6750497b9
Yaser Moradi <ysmoradi@outlook.com>
2021-06-09 19:44:26 +0430

    added additional steps

---
+++ web/step-02/src/SimpleCounter.Blazor/Pages/counter.razor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCounter.Blazor.Pages
+{
+    public partial class Counter
+    {
+        public int currentCount = 0;
+
+        public void Increase()
+        {
+            currentCount++;
+        }
+
+        public void Decrease()
+        {
+            currentCount--;
+        }
+    }
+}
