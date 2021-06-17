94a55e4db8919c1428bf3a0eaef96cfab248c072
Fateme Norouzi <f.noroozi90@gmail.com>
2021-06-17 03:29:28 -0700

    Update Calculator component

--- web/step-04/src/SimpleBlazorCalculator/Pages/Calculator.razor.cs
+++ web/step-04/src/SimpleBlazorCalculator/Pages/Calculator.razor.cs
@@ -1,4 +1,9 @@
-﻿namespace SimpleBlazorCalculator.Pages
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleBlazorCalculator.Pages
 {
     public partial class Calculator
     {
