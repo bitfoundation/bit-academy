8c5b277e0f69048d242a02fcafd0ad3aa4be127e
Fateme Norouzi <f.noroozi90@gmail.com>
2021-06-17 02:59:34 -0700

    Add step-04

---
+++ web/step-04/src/SimpleBlazorCalculator/Pages/Calculator.razor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleBlazorCalculator.Pages
+{
+    public partial class Calculator
+    {
+        string num1;
+        string num2;
+        string finalresult;
+
+        void AddNumbers()
+        {
+            finalresult = (Convert.ToDouble(num1) + Convert.ToDouble(num2)).ToString();
+        }
+
+        void SubtractNumbers()
+        {
+            finalresult = (Convert.ToDouble(num1) - Convert.ToDouble(num2)).ToString();
+        }
+
+        void MultiplyNumbers()
+        {
+            finalresult = (Convert.ToDouble(num1) * Convert.ToDouble(num2)).ToString();
+        }
+
+        void DivideNumbers()
+        {
+            if (Convert.ToDouble(num2) != 0)
+            {
+                finalresult = (Convert.ToDouble(num1) / Convert.ToDouble(num2)).ToString();
+            }
+            else
+            {
+                finalresult = "Cannot Divide by Zero";
+            }
+        }
+    }
+}
